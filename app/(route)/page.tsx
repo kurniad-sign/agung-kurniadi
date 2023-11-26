1f039a175c8aa5f50dcc579a4dd892f12bf7a7c1
kurniagung <agungkurniadi99@gmail.com>
2023-11-26 17:34:58 +0700

    initialize file project

---
+++ app/(route)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello World, Agung Kurniadi</h1>
+    </main>
+  );
+}
---
+++ lib/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local';
+
+export const generalSans = localFont({
+  src: '../public/fonts/GeneralSans-Variable.ttf',
+  display: 'swap',
+  variable: '--font-general-sans',
+});
