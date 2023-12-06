2a2e14fde585df7d3d5d2cf8b47b5ca28dbadb11
kurniagung <agungkurniadi99@gmail.com>
2023-12-06 14:53:17 +0700

    add typing groq queries home

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,8 +1,78 @@
-import { InferType, makeSafeQueryRunner, q } from 'groqd';
+import { InferType, q, sanityImage } from 'groqd';
+import { groq } from 'next-sanity';
 
 import { client } from './client';
+import { home } from './types';
 
-export const runQuery = makeSafeQueryRunner((query) => client.fetch(query));
+export const homeDataQuery = groq`
+  *[_type == "home"][1]{
+    _id,
+    about,
+    hero,
+    project_list{
+      _type,
+      title,
+      list[]->{
+        _id,
+        coverImage{
+          ...,
+          asset->{
+            metadata{
+              dimensions,
+              blurHash
+            },
+            url
+          }
+        },
+        description,
+        site,
+        "slug": slug.current,
+        title,
+        tags,
+      }
+    },
+    skills{
+      _type,
+      title,
+      list[]->{
+        _id,
+        logo{
+          ...,
+          asset->{
+            metadata{
+              dimensions,
+              blurHash
+            },
+            url
+          }
+        },
+        name,
+         "slug": slug.current
+      }
+    }
+  }
+`;
+
+export async function makeHomeQuery() {
+  return await client
+    .fetch(homeDataQuery)
+    .then((result) => (result ? home.parse(result) : null));
+}
+
+// GROQD Query Below
+const projectQuery = q('*')
+  .filter('_type == "project"')
+  .grab$({
+    _id: q.string(),
+    coverImage: sanityImage('coverImage'),
+    description: q.string(),
+    site: q.string(),
+    slug: q.slug('slug'),
+    title: q.string(),
+    tags: q.array(q.string()),
+  });
+
+type ProjectType = InferType<typeof projectQuery>;
 
 export const homeQuery = q('*')
   .filter('_type == "home"')
@@ -23,6 +93,7 @@ export const homeQuery = q('*')
     project_list: q.object({
       _type: q.string(),
       title: q.string(),
+      // list: projectQuery
     }),
     skills: q.object({
       _type: q.string(),
---
+++ sanity/lib/types.ts
@@ -0,0 +1,67 @@
+import { z } from 'zod';
+
+const Slug = z
+  .object({
+    current: z.string().nullable(),
+  })
+  .nullable();
+
+const Image = z.object({
+  _type: z.string(),
+  asset: z.object({
+    metadata: z.object({
+      dimensions: z.object({
+        width: z.number(),
+        height: z.number(),
+        aspectRatio: z.number(),
+        _type: z.string(),
+      }),
+      blurHash: z.string(),
+    }),
+    url: z.string(),
+  }),
+});
+
+const Project = z.object({
+  _id: z.string(),
+  coverImage: Image,
+  description: z.string(),
+  site: z.string(),
+  slug: z.string(),
+  title: z.string(),
+  tags: z.array(z.string()),
+});
+
+const Skill = z.object({
+  _id: z.string(),
+  name: z.string(),
+  slug: z.string(),
+  logo: Image,
+});
+
+export const home = z.object({
+  _id: z.string(),
+  about: z.object({
+    _type: z.string(),
+    description: z.string(),
+    title: z.string(),
+  }),
+  hero: z.object({
+    _type: z.string(),
+    description: z.string(),
+    scroll_text: z.string(),
+    title: z.string(),
+  }),
+  project_list: z.object({
+    _type: z.string(),
+    title: z.string(),
+    list: z.array(Project),
+  }),
+  skills: z.object({
+    _type: z.string(),
+    title: z.string(),
+    list: z.array(Skill),
+  }),
+});
+
+export type HomeQueryType = z.infer<typeof home>;
