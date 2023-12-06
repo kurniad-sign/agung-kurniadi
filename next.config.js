b3f6d60fe28a4819146a13c242793684b4f1830e
kurniagung <agungkurniadi99@gmail.com>
2023-12-06 14:52:36 +0700

    setup config image host for sanity

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: 'cdn.sanity.io' }],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
