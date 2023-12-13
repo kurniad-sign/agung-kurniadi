fb8cf75ed746ce8fedfb9d747709834b8200348e
kurniagung <agungkurniadi99@gmail.com>
2023-12-13 10:41:28 +0700

    add navigation animation

--- components/shared/navigation.tsx
+++ components/shared/navigation.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import { useState } from 'react';
 import Image from 'next/image';
 import LogoBlack from '@/public/images/logo-black.svg';
+import gsap from 'gsap';
 import { Dot, FileDown } from 'lucide-react';
 
+import { useIsomorphicLayoutEffect } from '@/hooks/use-isomorphic';
 import { NavigationType } from '@/sanity/types/navigation';
 
 export function Navigation({
@@ -11,6 +14,44 @@ export function Navigation({
 }: {
   data: NavigationType;
 }) {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      const hideTrigger = 100;
+      const showTrigger = 2000;
+
+      if (
+        currentScrollY > hideTrigger &&
+        currentScrollY < showTrigger &&
+        isNavVisible
+      ) {
+        gsap.to('.c-header', {
+          y: '-100%',
+          duration: 1,
+          ease: 'power3.inOut',
+        });
+        setIsNavVisible(false);
+      } else if (
+        (currentScrollY <= hideTrigger || currentScrollY >= showTrigger) &&
+        !isNavVisible
+      ) {
+        gsap.to('.c-header', {
+          y: '0%',
+          duration: 1,
+          ease: 'power3.inOut',
+        });
+        setIsNavVisible(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isNavVisible]);
+
   return (
     <header className="c-header">
       <nav className="c-nav">
