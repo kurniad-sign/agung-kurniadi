2f5edd54d425c10e410670b6d844c5db8ba46b0a
kurniagung <agungkurniadi99@gmail.com>
2023-12-14 20:16:28 +0700

    remove unused font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,33 +1,9 @@
 import localFont from 'next/font/local';
 
-export const generalSans = localFont({
-  src: '../public/fonts/GeneralSans-Variable.ttf',
-  display: 'swap',
-  variable: '--font-general-sans',
-});
-
-export const satoshi = localFont({
-  src: '../public/fonts/Satoshi-Variable.ttf',
-  display: 'swap',
-  variable: '--font-satoshi',
-});
-
-export const epilogue = localFont({
-  src: '../public/fonts/Epilogue-Variable.ttf',
-  display: 'swap',
-  variable: '--font-epilogue',
-});
-
 export const sora = localFont({
   src: '../public/fonts/Sora-Variable.ttf',
   display: 'swap',
   variable: '--font-sora',
-});
-
-export const switzer = localFont({
-  src: '../public/fonts/Switzer-Variable.ttf',
-  display: 'swap',
-  variable: '--font-switzer',
 });
 
 export const manrope = localFont({
